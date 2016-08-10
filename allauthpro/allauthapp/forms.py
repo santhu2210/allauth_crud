4394a0558282a4da0ad3648e1894ff538dbea3dd
Systag <systag@tag064.tag.dc>
2016-08-10 13:09:49 +0530

    edited forms.py

--- allauthpro/allauthapp/forms.py
+++ allauthpro/allauthapp/forms.py
@@ -3,16 +3,15 @@ from django import forms
 
 
 class EmpForm(forms.Form):
-	emp_name = models.CharField(max_length = 25,required=True)
-	emp_id = models.CharField(max_length = 5,unique=True,required=True )
-	email_id = models.CharField(max_length = 30,unique=True,required=True)
+	emp_name = forms.CharField()
+	emp_id = forms.CharField()
+	email_id = forms.CharField()
 	gender = (("MALE", "MALE"),("FEMALE", "FEMALE"))
-	gen = models.CharField(max_length=1,choices=gender,default="MALE",required=True)
+	gen = forms.ChoiceField(choices=gender)
 	department = (("OSA", "OSA"),("OSB", "OSB"),("MAINFRAME", "MAINFRAME"),("JAVA", "JAVA"),("GD", "GRAPHIC DESIGNER"))
-	dept = models.CharField(max_length = 15,choices=dept,default="OSA",required=True)
-	exp = models.CharField(max_length= 16,required=True)	
+	dept = forms.ChoiceField(choices=department)
+	exp = forms.CharField()	
 	skillset=(("JAVA","JAVA"),("PYTHON","PYTHON"),("RUBY","RUBY"))
 
-	skills = models.CharField(max_length= 50,choices=skillset,default="PYTHON",required=True)
-	fdbk = models.CharField(max_length = 500,required=True)
-	
+	skills = forms.ChoiceField(choices=skillset)
+	fdbk = forms.CharField()
